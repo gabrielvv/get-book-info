ce4c2739da0be4672e6e5c8ebf1da551fe30de6d
gabrielvv <gabrielvautrin@gmail.com>
2018-11-09 00:55:50 +0100

    upload controller

--- controllers/common.js
+++ controllers/common.js
@@ -1,14 +1,18 @@
 const util = require('util');
+// const Tesseract = require('tesseract.js')
 const multiparty = require('multiparty');
 
 exports.imageForm = function (req, res) {
     res.render('upload', {
-        title: 'Upload Images'
+        title: 'Upload Images',
     });
 };
 
 exports.uploadImage = function (req, res) {
-    var form = new multiparty.Form();
+    const form = new multiparty.Form(/*{
+        autoFiles: true,
+        uploadDir: __dirname + '../public/images',
+    }*/);
 
     form.parse(req, function (err, fields, files) {
         if (err) {
@@ -20,5 +24,24 @@ exports.uploadImage = function (req, res) {
         res.write('received fields:\n\n ' + util.inspect(fields));
         res.write('\n\n');
         res.end('received files:\n\n ' + util.inspect(files));
+        
+        /*
+        { image: 
+   [ { fieldName: 'image',
+       originalFilename: 'Le Dernier Empereur.jpg',
+       path: 'C:\\Users\\Gabriel\\AppData\\Local\\Temp\\3QYjL20RUvb68Sd98smeIcK9.jpg',
+       headers: [Object],
+       size: 1330318 } ] }
+       */
+
+        // Tesseract.recognize(files.image[0].path)
+        //     .then(function(result){
+        //         console.log(result)
+        //         res.end(result)
+        //     })
+        //     .catch(err => {
+        //         console.error(result)
+        //         res.end(err)
+        //     })
     });
 };
